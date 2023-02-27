828f10d85f87fa2c2f74c4ad55eee749a6ee9a92
daniloceano <danilo.oceano@gmail.com>
2023-02-27 19:16:09 -0300

    Feat: Lorenz validation usinh only main terms

--- validate_Lorenz.py
+++ validate_Lorenz.py
@@ -14,6 +14,7 @@ from sklearn.metrics import mean_squared_error
 import itertools
 import seaborn as sns
 import cmocean.cm as cmo
+from matplotlib.colors import LinearSegmentedColormap
 
 era_exp_name = 'ERA5-ERA5'
 
@@ -24,25 +25,18 @@ terms = ['Az', 'Ae', 'Kz', 'Ke',
          '∂Az/∂t (finite diff.)', '∂Ae/∂t (finite diff.)',
          '∂Kz/∂t (finite diff.)','∂Ke/∂t (finite diff.)',
          'RGz', 'RGe', 'RKz', 'RKe']
-
 energy_terms = ['Az','Ae','Kz','Ke']
-
-conversion_terms = ['Cz','Ca','Ck','Ce']
-
-boundary_terms = ['BAz', 'BAe', 'BKz', 'BKe']
-
 budget_diff_terms = ['∂Az/∂t (finite diff.)', '∂Ae/∂t (finite diff.)',
                  '∂Kz/∂t (finite diff.)', '∂Ke/∂t (finite diff.)']
-
 budget_diff_renamed = ['∂Az/∂t', '∂Ae/∂t',
                  '∂Kz/∂t', '∂Ke/∂t']
 
-residuals_terms = ['RGz', 'RKz', 'RGe', 'RKe']
+main_terms = ['Az', 'Ae', 'Kz', 'Ke', 'Ca', 'Ce', 'Ck', 'Ge']
 
-generation_terms = ['RGz', 'RGe', 'Gz', 'Ge']
 
 results_ERA = pd.read_csv('./LEC_Results_48h/ERA5-ERA5/ERA5-ERA5.csv')
 
+DiscreteColors = ['#58A8D6', '#74D669', '#D6BF5A', '#D6713A', '#D63631']
     
 def get_rsme_r(results, results_ERA):
 
@@ -83,18 +77,28 @@ def get_rsme_r(results, results_ERA):
     return df_rmse, df_corrcoef
 
 def sns_heatmap(data,title):
-    if 'RMSE' in title:
-        cmap = cmo.matter
+     
+    if 'RMSE_Normalised' in title:
+        cmap = LinearSegmentedColormap.from_list('Custom',
+                                    DiscreteColors, len(DiscreteColors))
+    elif 'R_Normalised' in title:
+        cmap = LinearSegmentedColormap.from_list('Custom',
+                                    DiscreteColors[::-1], len(DiscreteColors))
     else:
-        cmap = cmo.matter_r
+        if 'RMSE' in title:
+            cmap = cmo.matter
+        else:
+            cmap = cmo.matter_r
+    
     plt.close('all')
     f, ax = plt.subplots(figsize=(10, 10))
     sns.heatmap(data, annot=False, linewidths=.5, ax=ax, cmap=cmap)
+
+
     plt.title(title)
     plt.tight_layout()
     f.savefig('Figures_48h/stats_Lorenz/'+title)
    
-
 
 results = glob.glob('LEC_Results_48h/*')
 results_ERA = pd.read_csv('./LEC_Results_48h/ERA5-ERA5/ERA5-ERA5.csv')
@@ -103,58 +107,68 @@ for exp in results:
     if 'ERA5-ERA5' not in exp:
         exps.append(exp.split('/')[1].split('_MPAS_track')[0])
 
-df_rmse, df_corrcoef = get_rsme_r(results, results_ERA)
+df_rmse_all, df_corrcoef_all = get_rsme_r(results, results_ERA)
+
+df_rmse_main = df_rmse_all[main_terms]
+df_corrcoef_main = df_corrcoef_all[main_terms]
 
 
-# Print which terms had the worse general performance, for each metric
-df_rmse_energy = df_rmse[energy_terms]
-rmse_energy_thres = round(df_rmse_energy.max().max()/4,-4)
-print('Greater RMSE ( >= '+str(rmse_energy_thres)+'):',
-      df_rmse_energy.mean()[df_rmse_energy.mean() >=
-                                   rmse_energy_thres].index.tolist())
-
-df_rmse_not_energy = df_rmse.drop(columns=energy_terms)
-rmse_thres = round(df_rmse_not_energy.max().max()/2)
-print('Greater RMSE ( >= '+str(rmse_thres)+'):',
-     df_rmse_not_energy.mean()[df_rmse_not_energy.mean() >=
-                                   rmse_thres].index.tolist())
-
-print('Smaller correlations ( <= 0.6):',
-      df_corrcoef.mean()[df_corrcoef.mean() <= 0.6].index.tolist())
-
-# Normalize values for comparison
-rmse_norm = (df_rmse-df_rmse.min()
-                  )/(df_rmse.max()-df_rmse.min())   
-corrcoef_norm =  (df_corrcoef-df_corrcoef.min()
-                  )/(df_corrcoef.max()-df_corrcoef.min())   
-
-for data, title in zip([df_rmse_energy, df_rmse_not_energy,
-                        rmse_norm, df_corrcoef, corrcoef_norm],
-                       ['RMSE_energy', 'RMSE', 'RMSE_Normalised',
-                        'R', 'R_Normalised']):
-    sns_heatmap(data,title)
-
-
-rmse_vals = np.arange(0.6,-0.01,-0.05)
-r_vals = np.arange(0.6,1.01,0.05)
-
-for rmse_val, r_val in itertools.product(rmse_vals, r_vals):
+for df_rmse, df_corrcoef in zip([df_rmse_all, df_rmse_main],
+                                [df_corrcoef_all, df_corrcoef_main]):
     
-    rmse_val, r_val = round(rmse_val,2), round(r_val,2)
+    print('-------------------------\n')
+    print(df_rmse.columns.to_list())
     
-    approved_rmse = rmse_norm[rmse_norm <= rmse_val].dropna().index.to_list()
-    approved_r = corrcoef_norm[corrcoef_norm >= r_val].dropna().index.to_list()
-    
-    if len(approved_rmse) > 0 and len(approved_r) > 0:
-    
-        approved = list(approved_rmse)
-        approved.extend(x for x in approved_r if x not in approved)
-        
+    if len(df_rmse.columns) == 22:
+        fname = 'all'
     else:
-        
-        approved = []
+        fname = 'main'
+
+    # Print which terms had the worse general performance, for each metric
+    df_rmse_energy = df_rmse[energy_terms]
+    rmse_energy_thres = round(df_rmse_energy.max().max()/4,-4)
+    print('Greater RMSE ( >= '+str(rmse_energy_thres)+'):',
+          df_rmse_energy.mean()[df_rmse_energy.mean() >=
+                                       rmse_energy_thres].index.tolist())
     
-    if len(approved) > 0:
-                
-        print('\nrmse:', rmse_val, 'r:', r_val)
-        [print(i) for i in approved]
+    df_rmse_not_energy = df_rmse.drop(columns=energy_terms)
+    rmse_thres = round(df_rmse_not_energy.max().max()/2)
+    print('Greater RMSE ( >= '+str(rmse_thres)+'):',
+         df_rmse_not_energy.mean()[df_rmse_not_energy.mean() >=
+                                       rmse_thres].index.tolist())
+    
+    print('Smaller correlations ( <= 0.6):',
+          df_corrcoef.mean()[df_corrcoef.mean() <= 0.6].index.tolist())
+    
+    # Normalize values for comparison
+    rmse_norm = (df_rmse-df_rmse.min()
+                      )/(df_rmse.max()-df_rmse.min())   
+    corrcoef_norm =  (df_corrcoef-df_corrcoef.min()
+                      )/(df_corrcoef.max()-df_corrcoef.min())   
+    
+    for data, title in zip([df_rmse_energy, df_rmse_not_energy,
+                            rmse_norm, df_corrcoef, corrcoef_norm],
+                           ['RMSE_energy', 'RMSE', 'RMSE_Normalised',
+                            'R', 'R_Normalised']):
+        sns_heatmap(data,title+'_'+fname)
+    
+    
+    rmse_vals = np.arange(0,1,0.05)
+    r_vals = np.arange(0,1,0.05)
+    
+    for rmse_val, r_val in itertools.product(rmse_vals, r_vals):
+        
+        rmse_val, r_val = round(rmse_val,2), round(r_val,2)
+        
+        approved_rmse = rmse_norm[rmse_norm <= rmse_val].dropna().index.to_list()
+        approved_r = corrcoef_norm[corrcoef_norm >= r_val].dropna().index.to_list()
+    
+        if len(approved_rmse) > 0 and len(approved_r) > 0:
+            approved = list(approved_rmse)
+            approved.extend(x for x in approved_r if x not in approved)
+        else:
+            approved = []
+        
+        if len(approved) > 0 and len(approved) <=4:
+            print('\nrmse:', rmse_val, 'r:', r_val)
+            [print(i) for i in approved]
