f1e9e58444a847adad523436de74aa5ead7f390a
daniloceano <danilo.oceano@gmail.com>
2023-06-28 21:53:01 -0300

    Update: optimization

--- src/export_track.py
+++ src/export_track.py
@@ -5,67 +5,116 @@ Created on Thu Feb 16 18:43:19 2023
 
 @author: daniloceano
 """
-
+import os
+import dask
 import glob
-import argparse
 import f90nml
+import argparse
 import datetime
 
 import pandas as pd
 import numpy as np
 import xarray as xr
+import dask.array as da
 
 from metpy.units import units
-
 from wrf import interplevel
-
-from metpy.constants import g
-from metpy.constants import Rd
+from metpy.constants import g, Rd
 from metpy.calc import virtual_temperature
-
 from geopy.distance import geodesic
 
 lat_domain = slice(-20,-35)
 lon_domain = slice(-55,-30)
 
-def get_exp_name(bench):
+# Helper functions
+
+def get_experiment_name(bench):
+    """
+    Extracts the experiment name from the benchmark path.
+
+    Args:
+        benchmark_path (str): Path to the benchmark directory.
+
+    Returns:
+        str: Experiment name.
+    """
     expname = bench.split('/')[-1].split('run.')[-1]
     microp = expname.split('.')[0].split('_')[-1]
     cumulus = expname.split('.')[-1].split('_')[-1] 
     return microp+'_'+cumulus
 
-def get_times_nml(namelist,model_data):
-    ## Identify time range of simulation using namelist ##
-    # Get simulation start and end dates as strings
+def get_simulation_times(namelist,model_data):
+    """
+    Get the simulation times based on the given namelist and model data.
+
+    Parameters:
+    namelist (dict): A dictionary containing the namelist data.
+    model_data (pd.DataFrame): A DataFrame containing the model data.
+
+    Returns:
+    pd.DatetimeIndex: A Pandas DatetimeIndex object representing the simulation times.
+    """
     start_date_str = namelist['nhyd_model']['config_start_time']
     run_duration_str = namelist['nhyd_model']['config_run_duration']
-    # Convert strings to datetime object
     start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d_%H:%M:%S')
-    
-    run_duration = datetime.datetime.strptime(run_duration_str,'%d_%H:%M:%S')
-    # Get simulation finish date as object and string
-    finish_date  = start_date + datetime.timedelta(days=run_duration.day,
-                                                   hours=run_duration.hour)
-    ## Create a range of dates ##
-    times = pd.date_range(start_date,finish_date,periods=len(model_data.Time)+1)[1:]
+    run_duration = datetime.datetime.strptime(run_duration_str, '%d_%H:%M:%S')
+    finish_date = start_date + datetime.timedelta(days=run_duration.day, hours=run_duration.hour)
+    times = pd.date_range(start_date, finish_date, periods=len(model_data.Time) + 1)[1:]
     return times
 
-def open_dataset(bench, times=None):
-    data =  xr.open_dataset(bench+'/latlon.nc').sortby(
-        'latitude', ascending=False).sel(
-            latitude=lat_domain,longitude=lon_domain)
+def open_model_dataset(benchmark_path, times=None):
+    """
+    Open and retrieve a model dataset.
+
+    Args:
+        bench (str): The directory of the dataset.
+        times (Optional[List[str]]): A list of time values to select.
+
+    Returns:
+        xr.Dataset: The model dataset.
+    """
+    data_file = os.path.join(benchmark_path, 'latlon.nc')
+
+    with dask.config.set(array={'slicing': {'split_large_chunks': True}}):
+        data = xr.open_dataset(data_file)
+        
+    data = data.sortby('latitude', ascending=False).sel(latitude=lat_domain, longitude=lon_domain)
+
     if times:
         data = data.assign_coords({"Time":times})
+    
     return data
 
-def pressure_to_slp(pressure, z, zlevs):
+def convert_pressure_to_slp(pressure, z, zlevs):
+    """
+    Convert the given pressure to sea-level pressure.
+
+    Parameters:
+        pressure (DataArray): The pressure values.
+        z (DataArray): The heights.
+        zlevs (array-like): The levels.
+
+    Returns:
+        DataArray: The sea-level pressure.
+    """    
     pres_height = interplevel(pressure, z[:,:-1], zlevs)
     slp = pres_height.isel(level=1)
     return slp
 
-
 def surface_pressure_to_mslp(surface_pressure,
                              mean_virtual_temperature, surface_height):
+    """
+    Calculates the mean sea level pressure (MSLP) from surface pressure, mean 
+    virtual temperature, and surface height.
+
+    Parameters:
+        surface_pressure (float): The surface pressure in Pa.
+        mean_virtual_temperature (float): The mean virtual temperature in K.
+        surface_height (float): The surface height in m.
+
+    Returns:
+        float: The mean sea level pressure (MSLP) in Pa.
+    """    
     a = Rd / g
     _ = surface_height / (a * mean_virtual_temperature)
     _ = np.exp(_.metpy.dequantify())
@@ -73,10 +122,19 @@ def surface_pressure_to_mslp(surface_pressure,
     return mslp
 
 def get_track(slp, TimeIndexer):
+    """
+    Generate a track dataframe based on the given sea level pressure data and time indexer.
+
+    Args:
+        slp (xarray.Dataset): The sea level pressure data.
+        TimeIndexer (str): The name of the time indexer in the sea level pressure data.
+
+    Returns:
+        pandas.DataFrame: A dataframe containing longitude, latitude, and minimum value for each time step.
+    """
     min_var, times = [], []
     lats, lons = [], []
-    slp = slp.sortby('latitude', ascending=True
-                     ).sortby('longitude', ascending=True)
+    slp = slp.sortby('latitude', ascending=True).sortby('longitude', ascending=True)
     for t in slp[TimeIndexer]:
         datestr = pd.to_datetime(t.values)
         times.append(str(datestr))
@@ -84,16 +142,16 @@ def get_track(slp, TimeIndexer):
         if t == slp[TimeIndexer][0]:
             ivar = slp.sel({TimeIndexer:t})
         else:
-            ivar = slp.sel({TimeIndexer:t}
-                           ).sel(latitude=slice(lats[-1]-7.55,lats[-1]+7.5)
-                           ).sel(longitude=slice(lons[-1]-7.5,lons[-1]+7.5))
+            ivar = slp.sel({TimeIndexer: t}).sel(
+                latitude=slice(lats[-1] - 7.55, lats[-1] + 7.5),
+                longitude=slice(lons[-1] - 7.5, lons[-1] + 7.5))
         
         varmin = ivar.min()
         min_var.append(float(varmin))
         
         loc = ivar.argmin(dim=['latitude','longitude'])
-        lats.append(float(ivar['latitude'][loc['latitude']]))
-        lons.append(float(ivar['longitude'][loc['longitude']]))
+        lats.append(float(ivar['latitude'][loc['latitude'].compute()]))
+        lons.append(float(ivar['longitude'][loc['longitude'].compute()]))
     
     track = pd.DataFrame([lons, lats, min_var]).transpose()
     track.columns = ['lon','lat','min']
@@ -101,11 +159,20 @@ def get_track(slp, TimeIndexer):
     return track
 
 def calculate_distance(row):
+    """
+    Calculate the distance between two coordinates in kilometers.
+
+    Parameters:
+        row (pandas.Series): A row of data containing latitude and longitude values.
+
+    Returns:
+        float: The distance between the two coordinates in kilometers.
+    """
     start = (row['lat_ref'], row['lon_ref'])
     end = (row['lat_model'], row['lon_model'])
     return geodesic(start, end).km  
 
-if __name__ == '__main__':
+def main():
 
     ## Parser options ##
     parser = argparse.ArgumentParser()
@@ -117,8 +184,22 @@ if __name__ == '__main__':
                             help='''output name to append file''')
     parser.add_argument('-e','--ERA5', type=str, default=None,
                             help='''wether to validade with ERA5 data''')
-    args = parser.parse_args()
+    
+    # args = parser.parse_args()
 
+    args = parser.parse_args(['-bdir', '/p1-nemo/danilocs/mpas/MPAS-BR/benchmarks/Catarina_physics-test/Catarina_250-8km.physics-pbl_sst/',
+                              '-e', '/p1-nemo/danilocs/mpas/MPAS-BR/met_data/ERA5/DATA/2004/Catarina-PhysicsTest_ERA5.grib',
+                              '-odir', '../experiments_48h/tracks_48h_pbl'])
+
+    # Validate inputs
+    if not os.path.isdir(args.bench_directory):
+        print("Invalid benchmark directory path.")
+        return
+
+    if args.ERA5 and not os.path.isfile(args.ERA5):
+        print("Invalid ERA5 file path.")
+        return
+    
     ## Start the code ##
     benchs = glob.glob(args.bench_directory+'/run*')
     print(benchs)
@@ -129,33 +210,39 @@ if __name__ == '__main__':
 
     # open data and namelist
     print('openinning first bench just to get dates...')
-    model_data = open_dataset(benchs[0])
+    model_data = open_model_dataset(benchs[0])
     namelist = f90nml.read(glob.glob(namelist_path)[0])
-    times = get_times_nml(namelist,model_data.compute()).tolist()
+    times = get_simulation_times(namelist,model_data.compute()).tolist()
     first_day = datetime.datetime.strftime(times[0], '%Y-%m-%d %HZ')
     last_day = datetime.datetime.strftime(times[-1], '%Y-%m-%d %HZ')                      
     print('Analysis is from',first_day,'to',last_day)  
 
     print('\nOpening all data and putting it into a dictionary...')
-    mslp = xr.open_dataset(args.ERA5, engine='cfgrib',
-                    filter_by_keys={'typeOfLevel': 'surface'}
-                    ).sel(time=slice(times[0],times[-1]),
-                    latitude=slice(-20,-35),longitude=slice(-55,-30)).msl
-    mslp = (mslp * units(mslp.units)).metpy.convert_units('hPa')                    
-    era_track = get_track(mslp, 'time')
-    era_track.to_csv(args.output_directory+'track_ERA5.csv')             
-    print(args.output_directory+'/track_ERA5.csv saved')     
+    if 'grib' in os.path.splitext(args.ERA5)[1]:
+        mslp = xr.open_dataset(args.ERA5, engine='cfgrib',
+                            filter_by_keys={'typeOfLevel': 'surface'}
+                            ).sel(time=slice(times[0],times[-1]),
+                            latitude=slice(-20,-35),longitude=slice(-55,-30)).msl
+    else:
+        xr.open_dataset(args.ERA5).sel(time=slice(times[0],times[-1]),
+                        latitude=slice(-20,-35),longitude=slice(-55,-30)).msl
+        
+    mslp = (mslp * units(mslp.units)).metpy.convert_units('hPa')    
 
-    track_Cowan = pd.read_csv(args.output_directory+'/track_Cowan.csv', index_col=0)
-    track_Cowan_sliced = track_Cowan.loc[
-        slice(first_day,last_day)]                 
+    era_track = get_track(mslp, 'time')
+
+    era_track.to_csv(os.path.join(args.output_directory, 'track_ERA5.csv'))
+    print(f"{os.path.join(args.output_directory, 'track_ERA5.csv')} saved")  
+
+    track_Cowan = pd.read_csv(os.path.join(args.output_directory, 'track_Cowan.csv'), index_col=0)
+    track_Cowan_sliced = track_Cowan.loc[slice(first_day, last_day)]               
                         
     for bench in benchs:
-        experiment = get_exp_name(bench)
+        experiment = get_experiment_name(bench)
         print('\n',experiment)
         
         print('computing slp...')
-        model_data  = open_dataset(bench, times=times)
+        model_data  = open_model_dataset(bench, times=times)
         
         surface_pressure = model_data['surface_pressure'] * units.Pa
         surface_height = model_data['zgrid'].isel(nVertLevelsP1=0) * units.m
@@ -172,14 +259,20 @@ if __name__ == '__main__':
         
         print('getting track..')
         track = get_track(slp, 'Time')
-        
+
         df_dist = pd.DataFrame({
-        'lat_ref': era_track.lat, 'lon_ref': era_track.lon,
-        'lat_model': track.lat, 'lon_model': track.lon})
+            'lat_ref': era_track.lat,
+            'lon_ref': era_track.lon,
+            'lat_model': track.lat,
+            'lon_model': track.lon
+        })
         df_dist = df_dist.loc[track_Cowan_sliced.index]
             
-        track['distance'] =  df_dist.apply(
-            lambda row: calculate_distance(row), axis=1)
+        track['distance'] =  df_dist.apply(lambda row: calculate_distance(row), axis=1)
         
-        track.to_csv(args.output_directory+'/track_'+experiment+'.csv')   
-        print(args.output_directory+'/track_'+experiment+'.csv saved')
+        track.to_csv(os.path.join(args.output_directory, f'track_{experiment}.csv'))
+        print(f"{os.path.join(args.output_directory, f'track_{experiment}.csv')} saved")
+
+
+if __name__ == '__main__':
+    main()
