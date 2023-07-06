bb84e8964604fcbc39b1822fe71ad60bbb97870a
daniloceano <danilo.oceano@gmail.com>
2023-07-05 23:50:16 -0300

    Feat: argparse

--- src/validate_wind.py
+++ src/validate_wind.py
@@ -10,7 +10,7 @@ import glob
 import os
 import f90nml
 import datetime
-import itertools
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -92,141 +92,146 @@ def plot_taylor(sdevs,crmsds,ccoefs,experiments):
                       titleOBS = 'QUICKSCAT', styleObs =':',
                       alpha = 1)
 
-## Inputs ##
+def main(benchmarks_directory, benchmarks_name, experiment_directory, quickscat_file):
+    stats_directory = os.path.join(experiment_directory, f'stats_{benchmarks_name}')
+    figures_directory = os.path.join(experiment_directory, f'Figures_{benchmarks_name}')
 
-experiment_directory = '../experiments_48h'
-benchmarks_name = '48h_pbl'
+    ## Start the code ##
+    benchs = glob.glob(f"{benchmarks_directory}/run*")
 
-benchmarks_path = '/p1-nemo/danilocs/mpas/MPAS-BR/benchmarks/Catarina_physics-test/'
-#benchmarks_directory = f'{benchmarks_path}/Catarina_250-8km.microp_scheme.convection_scheme'
-benchmarks_directory = f'{benchmarks_path}/Catarina_250-8km.physics-pbl_sst/'
+    # Dummy for getting model times
+    model_output = benchs[0]+'/latlon.nc'
+    namelist_path = benchs[0]+"/namelist.atmosphere"
 
+    # Open data and namelist
+    model_data = xr.open_dataset(model_output)
+    namelist = f90nml.read(glob.glob(namelist_path)[0])
+    times = get_times_nml(namelist,model_data)
 
-quickscat_file = '/p1-nemo/danilocs/mpas/MPAS-BR/met_data/QUICKSCAT/Catarina_20040321-20040323_v11l30flk.nc'
-era5_file = '/p1-nemo/danilocs/mpas/MPAS-BR/met_data/ERA5/DATA/2004/Catarina-2103-2303_ERA5.nc'
+    first_day = datetime.datetime.strftime(times[0], '%Y-%m-%d')
+    last_day = datetime.datetime.strftime(times[-2], '%Y-%m-%d')
 
-stats_directory = os.path.join(experiment_directory, f'stats_{benchmarks_name}')
-figures_directory = os.path.join(experiment_directory, f'Figures_{benchmarks_name}')
+    # Reference: quickscat
+    da_reference = convert_lon(xr.open_dataset(quickscat_file),'lon').sel(
+                        lat=slice(model_data.latitude[-1],model_data.latitude[0]),
+                        lon=slice(model_data.longitude[0],model_data.longitude[-1])
+                        ).sel(time=slice(first_day,last_day))
+    u = da_reference.uwnd
+    v = da_reference.vwnd
+    reference = 'quickscat'
 
-## Start the code ##
-benchs = glob.glob(f"{benchmarks_directory}/run*")
+    # Reference: ERA5
+    # da_reference = convert_lon(xr.open_dataset(args.ERA5),'longitude').sel(
+    #                     latitude=slice(
+    #                         model_data.latitude[0],model_data.latitude[-1]),
+    #                     longitude=slice(
+    #                         model_data.longitude[0],model_data.longitude[-1])
+    #                     ).sel(time=slice(first_day,last_day))
+    # u = da_reference.u10
+    # v = da_reference.v10
+    # reference = 'ERA5'                          
 
-# Dummy for getting model times
-model_output = benchs[0]+'/latlon.nc'
-namelist_path = benchs[0]+"/namelist.atmosphere"
+    print('\nOpening all data and putting it into a dictionary...')
+    benchmarks = [BenchData(bench, times) for bench in benchs]
+    data = {}
 
-# Open data and namelist
-model_data = xr.open_dataset(model_output)
-namelist = f90nml.read(glob.glob(namelist_path)[0])
-times = get_times_nml(namelist,model_data)
-
-first_day = datetime.datetime.strftime(times[0], '%Y-%m-%d')
-last_day = datetime.datetime.strftime(times[-2], '%Y-%m-%d')
-
-# Reference: quickscat
-da_reference = convert_lon(xr.open_dataset(quickscat_file),'lon').sel(
-                    lat=slice(model_data.latitude[-1],model_data.latitude[0]),
-                    lon=slice(model_data.longitude[0],model_data.longitude[-1])
-                    ).sel(time=slice(first_day,last_day))
-u = da_reference.uwnd
-v = da_reference.vwnd
-reference = 'quickscat'
-
-# Reference: ERA5
-# da_reference = convert_lon(xr.open_dataset(args.ERA5),'longitude').sel(
-#                     latitude=slice(
-#                         model_data.latitude[0],model_data.latitude[-1]),
-#                     longitude=slice(
-#                         model_data.longitude[0],model_data.longitude[-1])
-#                     ).sel(time=slice(first_day,last_day))
-# u = da_reference.u10
-# v = da_reference.v10
-# reference = 'ERA5'                          
-
-print('\nOpening all data and putting it into a dictionary...')
-benchmarks = [BenchData(bench, times) for bench in benchs]
-data = {}
-
-for benchmark in benchmarks:
-    exp_name = benchmark.get_exp_name()
-    print('\n',exp_name)
-    data[exp_name] = {}
-    
-    for model_var, reference_var in zip(['u10','v10','windspeed'],
-                                        [u,v,'windspeed']):
+    for benchmark in benchmarks:
+        exp_name = benchmark.get_exp_name()
+        print('\n',exp_name)
+        data[exp_name] = {}
         
-        data[exp_name][model_var] = {}
+        for model_var, reference_var in zip(['u10','v10','windspeed'],
+                                            [u,v,'windspeed']):
+            
+            data[exp_name][model_var] = {}
+            
+            if model_var == 'windspeed':
+                variable_experiment_data = wind_speed(benchmark.data['u10'],
+                                                benchmark.data['v10'])
+                reference_data = wind_speed(u,v)  
+                
+                
+            else:
+                variable_experiment_data = benchmark.data[model_var]
+                reference_data = reference_var
+                
+            interpoled_data = interpolate_mpas_data(
+                variable_experiment_data.compute(),reference_data, reference)
+            
+            data[exp_name][model_var]['data'] = variable_experiment_data
+            data[exp_name][model_var]['interpoled_data'] = interpoled_data
+
+            resampled_data = interpoled_data.sel(Time=reference_data.time)
+
+            statistical_metrics = sm.taylor_statistics(
+                resampled_data.values.ravel(), reference_data.values.ravel())
         
-        if model_var == 'windspeed':
-            variable_experiment_data = wind_speed(benchmark.data['u10'],
-                                               benchmark.data['v10'])
-            reference_data = wind_speed(u,v)  
-            
-            
-        else:
-            variable_experiment_data = benchmark.data[model_var]
-            reference_data = reference_var
-            
-        interpoled_data = interpolate_mpas_data(
-            variable_experiment_data.compute(),reference_data, reference)
+            data[exp_name][model_var].update(
+                {metric: statistical_metrics[metric][1] 
+                for metric in statistical_metrics.keys()})
         
-        data[exp_name][model_var]['data'] = variable_experiment_data
-        data[exp_name][model_var]['interpoled_data'] = interpoled_data
+    # =============================================================================
+    # Plot Taylor Diagrams and do Statistics ##
+    # =============================================================================
 
-        resampled_data = interpoled_data.sel(Time=reference_data.time)
+    for var in ['u10','v10','windspeed']:
+        
+        print('\n-------------------------------')
+        print(var)
+        
 
-        statistical_metrics = sm.taylor_statistics(
-            resampled_data.values.ravel(), reference_data.values.ravel())
-    
-        data[exp_name][model_var].update(
-            {metric: statistical_metrics[metric][1] 
-             for metric in statistical_metrics.keys()})
-    
-# =============================================================================
-# Plot Taylor Diagrams and do Statistics ##
-# =============================================================================
+        fname = f'{benchmarks_name}_{var}'
+        
+        ccoef = [data[exp][var]['ccoef'] for exp in data.keys() if exp != 'IMERG']
+        crmsd  = [data[exp][var]['crmsd'] for exp in data.keys() if exp != 'IMERG']
+        sdev = [data[exp][var]['sdev'] for exp in data.keys() if exp != 'IMERG']
+        ccoef, crmsd, sdev = np.array(ccoef),np.array(crmsd),np.array(sdev)
+        print('plotting taylor diagrams..')
+        fig = plt.figure(figsize=(10,10))
+        plot_taylor(sdev,crmsd,ccoef,list(data.keys()))
+        plt.tight_layout(w_pad=0.1)
 
-for var in ['u10','v10','windspeed']:
-    
-    print('\n-------------------------------')
-    print(var)
-    
+        outname = f'{figures_directory}/stats_wind/{fname}-taylor.png'
+        fig.savefig(outname, dpi=500)    
+        print(f'Saved {outname}')
+        
+        
+        df_stats = pd.DataFrame(crmsd,
+                            index=[exp for exp in data.keys() if exp != 'IMERG'],
+                            columns=['rmse'])
+        df_stats['ccoef'] = ccoef
+        
+        # Normalize values for comparison
+        df_stats_norm = (df_stats-df_stats.min())/(df_stats.max()-df_stats.min())
+        fname_stats = f'{stats_directory}/{var}_RMSE_normalised.csv'
+        df_stats_norm.sort_index(ascending=True).to_csv(fname_stats)
+        print(f'Saved {fname_stats}') 
+        
+        for df, title in zip([df_stats, df_stats_norm],
+                            ['stats', 'stats normalised']):
+            for col in df.columns:
+                plt.close('all')
+                f, ax = plt.subplots(figsize=(10, 10))
+                ax.bar(df.index,df[col].values)
+                plt.xticks(rotation=30, ha='right')
+                plt.tight_layout()
+                outname = f'{figures_directory}/stats_wind/{title}_{col}.png'
+                f.savefig(outname, dpi=500)
+                print(f'Saved {outname}')
 
-    fname = f'{benchmarks_name}_{var}'
-    
-    ccoef = [data[exp][var]['ccoef'] for exp in data.keys() if exp != 'IMERG']
-    crmsd  = [data[exp][var]['crmsd'] for exp in data.keys() if exp != 'IMERG']
-    sdev = [data[exp][var]['sdev'] for exp in data.keys() if exp != 'IMERG']
-    ccoef, crmsd, sdev = np.array(ccoef),np.array(crmsd),np.array(sdev)
-    print('plotting taylor diagrams..')
-    fig = plt.figure(figsize=(10,10))
-    plot_taylor(sdev,crmsd,ccoef,list(data.keys()))
-    plt.tight_layout(w_pad=0.1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-    outname = f'{figures_directory}/stats_wind/{fname}-taylor.png'
-    fig.savefig(outname, dpi=500)    
-    print(f'Saved {outname}')
-    
-    
-    df_stats = pd.DataFrame(crmsd,
-                           index=[exp for exp in data.keys() if exp != 'IMERG'],
-                           columns=['rmse'])
-    df_stats['ccoef'] = ccoef
-    
-    # Normalize values for comparison
-    df_stats_norm = (df_stats-df_stats.min())/(df_stats.max()-df_stats.min())
-    fname_stats = f'{stats_directory}/{var}_RMSE_normalised.csv'
-    df_stats_norm.sort_index(ascending=True).to_csv(fname_stats)
-    print(f'Saved {fname_stats}') 
-    
-    for df, title in zip([df_stats, df_stats_norm],
-                           ['stats', 'stats normalised']):
-        for col in df.columns:
-            plt.close('all')
-            f, ax = plt.subplots(figsize=(10, 10))
-            ax.bar(df.index,df[col].values)
-            plt.xticks(rotation=30, ha='right')
-            plt.tight_layout()
-            outname = f'{figures_directory}/stats_wind/{title}_{col}.png'
-            f.savefig(outname, dpi=500)
-            print(f'Saved {outname}')
+    parser.add_argument("benchmarks_directory", nargs="?",
+                        default='/p1-nemo/danilocs/mpas/MPAS-BR/benchmarks/Catarina_physics-test/Catarina_250-8km.microp_scheme.convection_scheme',
+                        help="Path to MPAS benchmarks")
+    parser.add_argument("benchmarks_name", nargs="?",default="48h",
+                        help="Name to use for this set of experiments")
+    parser.add_argument("experiment_directory", nargs="?",default='../experiments_48h',
+                        help="Path to expeirment directory for saving results")
+    parser.add_argument("quickscat_file", nargs="?",
+                        default='/p1-nemo/danilocs/mpas/MPAS-BR/met_data/QUICKSCAT/Catarina_20040321-20040323_v11l30flk.nc',
+                        help="Path to QUICKSCAT file")
+    args = parser.parse_args()
+
+    main(args.benchmarks_directory, args.benchmarks_name, args.experiment_directory, args.quickscat_file)
