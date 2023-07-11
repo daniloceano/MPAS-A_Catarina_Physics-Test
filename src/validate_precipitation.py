c32b0c2afb0e984ec5c44267131ea94ac6c124df
daniloceano <danilo.oceano@gmail.com>
2023-07-11 11:17:40 -0300

    UPdate: removing extend

--- src/validate_precipitation.py
+++ src/validate_precipitation.py
@@ -6,7 +6,7 @@
 #    By: Danilo  <danilo.oceano@gmail.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/08 09:52:10 by Danilo            #+#    #+#              #
-#    Updated: 2023/07/11 11:13:53 by Danilo           ###   ########.fr        #
+#    Updated: 2023/07/11 11:17:31 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -359,7 +359,7 @@ def plot_precipitation_panels(
                 bias = prec_interp-imerg_accprec
                 cf = ax.contourf(imerg_accprec.lon, imerg_accprec.lat,bias,
                                     cmap=cmap_bias, levels=prec_levels,
-                                    norm=bias_norm, extend='both')
+                                    norm=bias_norm)
             ax.coastlines(zorder = 1)
             i+=1
 
